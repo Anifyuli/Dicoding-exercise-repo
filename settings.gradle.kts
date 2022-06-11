69e16e15ec56af752a1b8a973d3d92f941f52192
anifyuli <anifyuliansyah@gmail.com>
2022-06-11 21:55:52 +0700

    Push to update

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Dicoding-class"
+
---
+++ src/main/kotlin/RawString.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val poem = """
+        Aku hanyalah insan
+        Yang tak luput dari kesalahan
+        Berusaha berdamai dengan masa lalu
+        Masa yang penuh dengan egoisme diri
+    """.trimIndent()
+    
+    println(poem)
+}
